f2756cc9196f91a2ee8105b3854c8659216bdeb7
Eren Kaya <erenkaya1993@gmail.com>
2018-05-14 15:13:04 +0300

    new certificated module added.

---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/App.java
@@ -0,0 +1,13 @@
+package kim.eren.oca_8_certificate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
