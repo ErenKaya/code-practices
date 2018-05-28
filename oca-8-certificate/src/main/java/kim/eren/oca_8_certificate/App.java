6ecb04dccf946d46f4f8c37a609deae3ec2d322a
Eren Kaya <erenkaya1993@gmail.com>
2018-05-28 23:42:57 +0300

    Book question solve

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/App.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/App.java
@@ -4,10 +4,14 @@ package kim.eren.oca_8_certificate;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		String java= "java";
+		String java1 = "java";
+		StringBuilder sb = new StringBuilder();
+		sb.append("ja").append("va");
+		
+		System.out.println(sb.toString() == java);
+		System.out.println(java == java1);
+	}
 }
