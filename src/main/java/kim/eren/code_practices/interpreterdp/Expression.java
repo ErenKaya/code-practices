fad84077a1a88c5174024ba4576a110dd540bc5f
Eren Kaya <erenkaya1993@gmail.com>
2018-06-13 23:27:13 +0300

    merge

---
+++ src/main/java/kim/eren/code_practices/interpreterdp/Expression.java
@@ -0,0 +1,5 @@
+package kim.eren.code_practices.interpreterdp;
+
+public interface Expression {
+	public boolean interpret(String context);
+}
---
+++ src/main/java/kim/eren/code_practices/interpreterdp/TerminalExpression.java
@@ -0,0 +1,19 @@
+package kim.eren.code_practices.interpreterdp;
+
+public class TerminalExpression implements Expression {
+
+	public String data;
+
+	public TerminalExpression(String data) {
+		this.data = data;
+	}
+
+	public boolean interpret(String context) {
+		if (context.contains(data)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
