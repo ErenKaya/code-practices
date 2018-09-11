dda7559b31bcda3595463954285b7a2f31f661d4
erenkaya <erenkaya1993@gmail.com>
2018-09-11 10:28:49 +0300

    regular css test proj added

---
+++ react-course-mead/boiler-plate - css/src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.css';
+
+ReactDOM.render(<p className="input-container">This is my boilerplate</p>, document.getElementById('app'));
