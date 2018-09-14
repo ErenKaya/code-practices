ee2c40e0e722bee93d8bf293039049e1e3cda0e7
Eren Kaya <erenkaya1993@gmail.com>
2018-09-14 22:56:12 +0300

    Optional example variable added i should tests optional example

---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/lambda/mmullerbook/chapter_6/OptionalExample.java
@@ -0,0 +1,7 @@
+package kim.eren.oca_8_certificate.lambda.mmullerbook.chapter_6;
+
+import java.util.Optional;
+
+public class OptionalExample {
+	public Optional<String> strExample;
+}
---
+++ oca-8-certificate/src/test/java/kim/eren/oca_8_certificate/lambda/mmullerbook/chapter_6/OptionalTest.java
@@ -0,0 +1,23 @@
+package kim.eren.oca_8_certificate.lambda.mmullerbook.chapter_6;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class OptionalTest {
+
+	@Test
+	/**
+	 * I should test another case i didn't understand what do you mean with optional object.
+	 */
+	public void test_optional_string() {
+//		OptionalExample optionalExample = new OptionalExample();
+//		boolean optionalResult = false;
+//		if(optionalExample.strExample.isPresent()) {
+//			optionalResult = true;
+//		}
+//		
+//		assertTrue(optionalResult);
+
+	}
+}
