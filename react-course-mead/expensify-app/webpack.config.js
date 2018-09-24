9174faee769a370f663d1e1d894703139671c271
Eren Kaya <erenkaya1993@gmail.com>
2018-09-24 23:10:52 +0300

    react-book redux sections

--- react-course-mead/expensify-app/webpack.config.js
+++ react-course-mead/expensify-app/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/playground/react-book.js',
     output: {
         path: path.join(__dirname, 'public/scripts'),
         filename: 'bundle.js'
