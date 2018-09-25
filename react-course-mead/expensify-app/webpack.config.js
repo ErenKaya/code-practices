d49e378e9e12efe554bfefc009b1a6e4c9375c84
erenkaya <erenkaya1993@gmail.com>
2018-09-25 09:19:45 +0300

    conflict test

--- react-course-mead/expensify-app/webpack.config.js
+++ react-course-mead/expensify-app/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/playground/redux-book.js',
     output: {
         path: path.join(__dirname, 'public/scripts'),
         filename: 'bundle.js'
