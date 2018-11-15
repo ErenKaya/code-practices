c7d7ad82c63b0391dac68127b18f201fdafb3352
Eren Kaya <erenkaya1993@gmail.com>
2018-11-15 23:27:52 +0300

    variables completed but exercises didnt done yet

---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/core/jt/languagebasic/variables/Arrays.java
@@ -0,0 +1,39 @@
+package kim.eren.oca_8_certificate.core.jt.languagebasic.variables;
+
+/**
+ * <ul>
+ * <li>Searching an array for a specific value to get the index at which it is
+ * placed (the <code>binarySearch</code> method).</li>
+ * <li>Comparing two arrays to determine if they are equal or not (the
+ * <code>equals</code> method).</li>
+ * <li>Filling an array to place a specific value at each index (the
+ * <code>fill</code> method).</li>
+ * <li>Sorting an array into ascending order. This can be done either
+ * sequentially, using the <code>sort</code> method, or concurrently, using the
+ * <code>parallelSort</code> method introduced in Java SE 8. Parallel sorting of
+ * large arrays on multiprocessor systems is faster than sequential array
+ * sorting.</li>
+ * </ul>
+ * 
+ * @author eren
+ *
+ */
+public class Arrays extends Variables {
+	// Alternatively, you can use the shortcut syntax to create and initialize an
+	// array:
+	int[] anArray = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
+
+	public static void main(String[] args) {
+		/**
+		 * System object has a method which is makes clone from your source array.
+		 */
+		// System.arraycopy(src, srcPos, dest, destPos, length);
+
+		char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e', 'i', 'n', 'a', 't', 'e', 'd' };
+
+		char[] copyTo = java.util.Arrays.copyOfRange(copyFrom, 2, 9);
+
+		System.out.println(new String(copyTo));
+	}
+
+}
---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/core/jt/languagebasic/variables/PrimitiveDataTypes.java
@@ -0,0 +1,56 @@
+package kim.eren.oca_8_certificate.core.jt.languagebasic.variables;
+
+/**
+ * * Primitive Data Types
+ * </p>
+ * <li>short</li>
+ * <p>
+ * It has a minimum value of -32,768 and a maximum value of 32,767 (inclusive).
+ * </p>
+ * <li>int</li>
+ * <p>
+ * In Java SE 8 and later, you can use the int data type to represent an
+ * unsigned 32-bit integer,
+ * </p>
+ * <li>long</li>
+ * <p>
+ * Java SE 8 and later, you can use the long data type to represent an unsigned
+ * 64-bit long,
+ * </p>
+ * <li>float</li>
+ * <p>
+ * The float data type is a single-precision 32-bit IEEE 754 floating point
+ * </p>
+ * <li>double</li>
+ * <p>
+ * The double data type is a double-precision 64-bit IEEE 754 floating point.
+ * </p>
+ * <li>boolean</li>
+ * <p>
+ * The boolean data type has only two possible values: true and false.
+ * </p>
+ * <li>char</li>
+ * <p>
+ * The char data type is a single 16-bit Unicode character. It has a minimum
+ * value of '\u0000' (or 0) and a maximum value of '\uffff' (or 65,535
+ * inclusive).
+ * </p>
+ * <ul>
+ * <h3>String</h3>
+ * <p>
+ * In addition to the eight primitive data types listed above, the Java
+ * programming language also provides special support for character strings via
+ * the java.lang.String class. Enclosing your character string within double
+ * quotes will automatically create a new String object; for example, String s =
+ * "this is a string";. String objects are immutable, which means that once
+ * created, their values cannot be changed. The String class is not technically
+ * a primitive data type, but considering the special support given to it by the
+ * language, you'll probably tend to think of it as such.
+ * </p>
+ * 
+ * @author eren
+ *
+ */
+public class PrimitiveDataTypes extends Variables {
+
+}
