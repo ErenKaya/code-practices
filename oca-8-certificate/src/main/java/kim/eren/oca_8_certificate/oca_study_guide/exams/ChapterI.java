54c5566c497d99ebfc51aa019bb573ee4d9e15dc
Eren Kaya <erenkaya1993@gmail.com>
2018-11-17 22:22:12 +0300

    chapter1 exam from study guide book

---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/exams/ChapterI.java
@@ -0,0 +1,8 @@
+package kim.eren.oca_8_certificate.oca_study_guide.exams;
+
+public class ChapterI {
+	/*
+	 * public class a {int count; public void a(){count=4;}public static void
+	 * main(String args[]){a ab = new a();System.out.println(ab.count);}}
+	 */
+}
