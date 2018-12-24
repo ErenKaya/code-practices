7b657f2907925925782c1f0237915fb784378b38
Eren Kaya <erenkaya1993@gmail.com>
2018-12-25 01:03:10 +0300

    some oca certificate problem solve

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter2/Casting.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter2/Casting.java
@@ -11,5 +11,22 @@ public class Casting {
 	 * declare as a short we should cast to smaller data
 	 */
 	short z = (short) (x * y);
+	/**
+	 * that line doesn't compile
+	 */
+	// String message = 10>10? "Greater then x":false;
+
+	long yT = 4;
+	/**
+	 * that line also doesn't compile because yT was long so declared value have to
+	 * be long!
+	 */
+	// int xT = 5 * yT;
+
+	byte yB = 50, xB = 40;
+	/**
+	 * that line doesn't compile
+	 */
+	// byte resB = (byte) yB + xB;
 
 }
