0e6b973a3a647a0da6747627f6acc7291711785c
Eren Kaya <erenkaya1993@gmail.com>
2019-01-06 02:01:39 +0300

    string and strinbuilder extra

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter3/sb/StringBuilderTest.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter3/sb/StringBuilderTest.java
@@ -2,11 +2,6 @@ package kim.eren.oca_8_certificate.oca_study_guide.chapter3.sb;
 
 public class StringBuilderTest {
 	public static void main(String[] args) {
-		StringBuilder a = new StringBuilder("abc");
-		StringBuilder b = a.append("de");
-		b = b.append("f").append("g");
-		System.out.println("a=" + a);
-		System.out.println("b=" + b);
 		/*
 		 * Did you say both print "abcdefg" ? Good. There’s only one StringBuilder
 		 * object here. We know that because new StringBuilder() was called only once.
@@ -16,5 +11,30 @@ public class StringBuilderTest {
 		 * assignment back to b does absolutely nothing. b is already pointing to that
 		 * StringBuilder .
 		 */
+
+		StringBuilder a = new StringBuilder("abc");
+		StringBuilder b = a.append("de");
+		b = b.append("f").append("g");
+		System.out.println("a=" + a);
+		System.out.println("b=" + b);
+
+		/**
+		 * Notice that substring() returns a String rather than a StringBuilder . That
+		 * is why sb is not changed. substring() is really just a method that inquires
+		 * about where the substring happens to be.
+		 */
+
+		StringBuilder sb = new StringBuilder("animals");
+		String sub = sb.substring(sb.indexOf("a"), sb.indexOf("al"));
+		int len = sb.length();
+		char ch = sb.charAt(6);
+		System.out.println(sub + " " + len + " " + ch); // anim 7 s
+
+		StringBuilder sb2 = new StringBuilder("abcdef");
+		sb2.delete(1, 3);
+		System.out.println(sb2);
+		// sb = adef
+//		sb2.deleteCharAt(5);
+		// throws an exception
 	}
 }
