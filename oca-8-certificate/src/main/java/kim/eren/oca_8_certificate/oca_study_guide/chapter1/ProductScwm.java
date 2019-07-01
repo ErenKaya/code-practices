e3bd2c565059eeec62581422ef4723f6ce2bcd77
Eren Kaya <erenkaya1993@gmail.com>
2019-07-01 22:53:53 +0300

    chapter one done but test have not be done

---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter1/ProductScwm.java
@@ -0,0 +1,6 @@
+package kim.eren.oca_8_certificate.oca_study_guide.chapter1;
+
+public class ProductScwm {
+	private String strTest;
+
+}
---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter1/SameClassWithMain.java
@@ -0,0 +1,12 @@
+package kim.eren.oca_8_certificate.oca_study_guide.chapter1;
+
+public class SameClassWithMain {
+	private String strTest;
+	private boolean boolTest;
+
+	public static void main(String[] args) {
+		SameClassWithMain scwm = new SameClassWithMain();
+		ProductScwm pScwm = new ProductScwm();
+		System.err.println(scwm.strTest + String.valueOf(scwm.boolTest));
+	}
+}
