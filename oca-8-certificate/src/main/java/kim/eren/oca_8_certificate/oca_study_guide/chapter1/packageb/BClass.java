dcba5feffb3bffb35f1ee58e4f46c018acd8a9d8
Eren Kaya <erenkaya1993@gmail.com>
2019-10-30 11:48:39 +0300

    some compile practiceses

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter1/packageb/BClass.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter1/packageb/BClass.java
@@ -3,8 +3,8 @@ package kim.eren.oca_8_certificate.oca_study_guide.chapter1.packageb;
 import kim.eren.oca_8_certificate.oca_study_guide.chapter1.packagea.AClass;
 
 public class BClass {
-    public static void main(String...args){
- AClass a;
- System.out.println("you got it");
-    }
+	public static void main(String... args) {
+		AClass a;
+		System.out.println("you got it");
+	}
 }
