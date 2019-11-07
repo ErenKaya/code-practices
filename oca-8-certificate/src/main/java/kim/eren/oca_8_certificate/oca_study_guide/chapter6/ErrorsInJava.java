6c38ccf2863e2bbf412124f722991e1fae24f9e3
Eren Kaya <erenkaya1993@gmail.com>
2019-11-07 06:57:00 +0300

    exam solved

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter6/ErrorsInJava.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter6/ErrorsInJava.java
@@ -1,9 +1,17 @@
 package kim.eren.oca_8_certificate.oca_study_guide.chapter6;
 
 public class ErrorsInJava{
+
+    
     static {
         int[] countsOfMoose = new int[3];
         int num = countsOfMoose[-1];
         }
-        public static void main(String[] args) { }
+        public static void main(String[] args) {int num = 2;  }
+        
+        
+        //stack over flow error
+        public static void doNotCodeThis(int num) {
+            doNotCodeThis(1);
+            }
 }
--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/exams/ChapterI.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/exams/ChapterI.java
@@ -32,6 +32,8 @@ public class ChapterI {
 	 * public class a {int count; public void a(){count=4;}public static void
 	 * main(String args[]){a ab = new a();System.out.println(ab.count);}}
 	 */
-	
+	/**
+	 * DONE
+	 */
 
 }
