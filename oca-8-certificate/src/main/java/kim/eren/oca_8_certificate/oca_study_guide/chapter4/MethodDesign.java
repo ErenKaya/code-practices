6fab725d1568c07e158442069d5300abc204f5f7
Eren Kaya <erenkaya1993@gmail.com>
2019-11-13 07:43:12 +0300

    test done

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter4/MethodDesign.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter4/MethodDesign.java
@@ -1,11 +1,14 @@
 package kim.eren.oca_8_certificate.oca_study_guide.chapter4;
 public class MethodDesign extends MethodDesignParent{
+    protected int a=2;
     public MethodDesign(){
+        this(2);
         System.out.println("child executed");
     }
 
     public MethodDesign(int a){
         super(a);
+        this.a = a;
         System.out.println("child executed+"+a);
     }
     public static void walk(int a, int... aList){
@@ -16,18 +19,26 @@ public class MethodDesign extends MethodDesignParent{
         System.out.println(param);
     }
 
-    public void autoBoxing(Integer param){
+    public int autoBoxing(Integer param){
         System.out.println(param);
+        return 0;
     }
 
-    public static void main(String[] args){
+    public static void objectPassByValueTest(MethodDesign methodDesign){
+        methodDesign = new MethodDesign(7);
+        methodDesign.a = 5;
+    }
+
+    public static final void main(String[] args){
         walk(1);
         walk(1,2);
         walk(1,2,3);
         walk(1,new int[]{1,2});
 
         int arg = 2;
-        MethodDesign md = new MethodDesign(1);
+        final MethodDesign md = new MethodDesign(1);
+        objectPassByValueTest(md);
+        System.out.println("methoddesign md"+md.a);
         // actually you can not create one more same method
         // because of java already autobox that primitive to wrapper class
         md.autoBoxing(arg);
---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/exams/book/TestVI.java
@@ -0,0 +1,5 @@
+package kim.eren.oca_8_certificate.oca_study_guide.exams.book;
+
+public class TestVI {
+   
+}
