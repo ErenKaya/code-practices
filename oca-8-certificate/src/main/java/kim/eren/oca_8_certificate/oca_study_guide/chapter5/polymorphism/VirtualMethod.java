7e08cade66ac2edf2e1be084132c4e2dda20f4a4
Eren Kaya <erenkaya1993@gmail.com>
2019-11-17 11:38:11 +0300

    virtual method test

---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter5/polymorphism/VirtualMethod.java
@@ -0,0 +1,28 @@
+package kim.eren.oca_8_certificate.oca_study_guide.chapter5.polymorphism;
+
+import javax.activation.MailcapCommandMap;
+
+public class VirtualMethod extends VirtualMethodParent{
+    // if you declare a method in parent class and uses overriden method 
+ // output shows that child method output
+
+    public String getName(){
+        return "VirtualMethodChild";
+    }
+
+    public static void main(String[] args) {
+        VirtualMethod vm = new VirtualMethod();
+        vm.showResult();
+    }
+}
+
+class VirtualMethodParent{
+
+    public String getName(){
+        return "Unknown";
+    }
+
+    public void showResult(){
+        System.out.println("result : "+getName());
+    }
+}
