cd5ba995bf70b22b56a650ce635b1daca28fcd4e
Eren Kaya <erenkaya1993@gmail.com>
2019-11-24 12:11:50 +0300

    mala gupta exam guide package created

---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/java_basics/Info.java
@@ -0,0 +1,8 @@
+package kim.eren.oca_8_certificate.mala_gupta_guide.java_basics;
+
+public class Info{
+    /**
+     * all information on the notebook
+     * that class created for purpose of "do not break the chain" 
+     */
+}
