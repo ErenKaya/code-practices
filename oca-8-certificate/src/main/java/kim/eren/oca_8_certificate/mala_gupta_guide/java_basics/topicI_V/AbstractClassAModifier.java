22b402d4c537a6f1ebbd613949d4c17aa7421f9c
Eren Kaya <erenkaya1993@gmail.com>
2019-11-25 07:25:24 +0300

    accessible test nonaccess modifiers test

---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/java_basics/topicI_V/AbstractClassAModifier.java
@@ -0,0 +1,10 @@
+package kim.eren.oca_8_certificate.mala_gupta_guide.java_basics.topicI_V;
+
+public abstract class AbstractClassAModifier {
+
+
+    // you can add protected access modifier to in public abstract class because of if 
+    // abstract class extends from another concrete class that action was a extending 
+    // operation so protected method is accessible for child class.
+    protected abstract void selam();
+}
