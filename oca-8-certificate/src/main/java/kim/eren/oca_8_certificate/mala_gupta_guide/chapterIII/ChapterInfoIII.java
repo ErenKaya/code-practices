53b0027bb7d443929cff2090be93c764afa84a63
Eren Kaya <erenkaya1993@gmail.com>
2019-11-27 07:50:07 +0300

    return statement test

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterIII/ChapterInfoIII.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterIII/ChapterInfoIII.java
@@ -1,5 +1,9 @@
 package kim.eren.oca_8_certificate.mala_gupta_guide.chapterIII;
 
 public class ChapterInfoIII {
-    
+    void setState(int weight){
+        return;
+        // that was not warning its compile error which description is 'unreachable code'
+        //int s = weight;
+    }
 }
