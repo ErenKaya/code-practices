27ad1b7f5c20823433becd208885093faf27c266
Eren Kaya <erenkaya1993@gmail.com>
2019-11-29 07:42:39 +0300

    chapter Iv stringbuilder, string some method tests

---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterIV/topicI/StringMethods.java
@@ -0,0 +1,9 @@
+package kim.eren.oca_8_certificate.mala_gupta_guide.chapterIV.topicI;
+
+public class StringMethods{
+    public static void main(String[] args) {
+        String s = "eren";
+        s.charAt(0);
+        System.out.println(s.substring(0,1));
+    }
+}
