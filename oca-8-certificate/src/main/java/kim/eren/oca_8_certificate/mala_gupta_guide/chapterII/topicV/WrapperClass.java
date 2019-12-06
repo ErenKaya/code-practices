8ac612de67ad51b843d10f9899df0e8f3c708767
Eren Kaya <erenkaya1993@gmail.com>
2019-12-06 09:20:20 +0300

    wrapper cash mechanism

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterII/topicV/WrapperClass.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterII/topicV/WrapperClass.java
@@ -3,6 +3,7 @@ package kim.eren.oca_8_certificate.mala_gupta_guide.chapterII.topicV;
 public class WrapperClass{
 
     public static void main(String[] args) {
+        // that was expected true because of wrapper class cash mechanism
         Long var1 = Long.valueOf(123);
         Long var2  = Long.valueOf("123");
         System.out.println(var1 == var2);
