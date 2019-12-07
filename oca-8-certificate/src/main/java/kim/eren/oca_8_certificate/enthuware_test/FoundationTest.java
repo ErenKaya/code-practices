f917d7d5f42077b216bff8c828051a603e7e8c57
Eren Kaya <erenkaya1993@gmail.com>
2019-12-07 21:24:49 +0300

    enthuware test begin

---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/enthuware_test/FoundationTest.java
@@ -0,0 +1,7 @@
+package kim.eren.oca_8_certificate.enthuware_test;
+
+public class FoundationTest{
+    /**
+     * half of test done
+     */
+}
--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter2/Statements.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/oca_study_guide/chapter2/Statements.java
@@ -1,11 +1,19 @@
 package kim.eren.oca_8_certificate.oca_study_guide.chapter2;
 
+public class Statements {
+	// Switch statements important things;
+	/**
+	 * you have to use constant variables if you want to use a variable in there you
+	 * have to modified as "final" also you can use enums
+	 */
 
-public class Statements{
-    // Switch statements important things;
-    /**
-     * you have to use constant variables 
-     * if you want to use a variable in there you have to modified as "final"
-     * also you can use enums
-     */
-} 
+	public static void main(String[] args) {
+		switch ("eren") {
+		case "Eren":
+			System.out.println();
+			break;
+		case "eren":
+		}
+	}
+
+}
