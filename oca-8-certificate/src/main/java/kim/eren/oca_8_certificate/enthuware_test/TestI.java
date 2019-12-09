6ab82f6da6390f0ba9c6bbf70958f25c41e748ec
Eren Kaya <erenkaya1993@gmail.com>
2019-12-09 07:32:27 +0300

    half of test done

---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/enthuware_test/TestI.java
@@ -0,0 +1,17 @@
+package kim.eren.oca_8_certificate.enthuware_test;
+
+
+public class TestI{
+    // we can use this on the declaration part
+    private String s = this.toString();
+    
+    {s = new String("s");
+    print(s);
+}
+
+
+    
+    void print(String s){
+        System.out.println(s);
+    }
+}
