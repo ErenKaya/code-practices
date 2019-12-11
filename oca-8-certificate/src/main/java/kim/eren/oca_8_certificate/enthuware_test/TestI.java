1681f3c9189e8862cf1fd5de7cf816df80fd2c3c
Eren Kaya <erenkaya1993@gmail.com>
2019-12-11 07:17:39 +0300

    testII 50 question answered

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/enthuware_test/TestI.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/enthuware_test/TestI.java
@@ -1,5 +1,7 @@
 package kim.eren.oca_8_certificate.enthuware_test;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestI{
     // we can use this on the declaration part
@@ -17,11 +19,12 @@ public class TestI{
 
     public static void main(String[] args) {
         // if your exception object have a message then to string method show it
-        System.out.println(new Exception("selam"));
+        System.out.println(3+5*9-7);
         int i1 = 1;
         int i2 = 1;
         if(i1 == i2){
             System.out.println("equal");
         }
+        List<String> stringList = new ArrayList<String>();
     }
 }
---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/enthuware_test/TestII.java
@@ -0,0 +1,7 @@
+package kim.eren.oca_8_certificate.enthuware_test;
+
+public class TestII{
+    public static void main(String[] args) {
+      
+    }
+}
