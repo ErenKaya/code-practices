5c3342fd27e22de7ce38fc7392c4ea38441596b5
Eren Kaya <erenkaya1993@gmail.com>
2019-12-12 07:55:12 +0300

    test done

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/enthuware_test/TestII.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/enthuware_test/TestII.java
@@ -1,7 +1,10 @@
 package kim.eren.oca_8_certificate.enthuware_test;
 
 public class TestII{
+    //TEST 2 DONE
     public static void main(String[] args) {
-      
+    //  if(true) break
+    //   else System.out.println("sd");
+
     }
 }
