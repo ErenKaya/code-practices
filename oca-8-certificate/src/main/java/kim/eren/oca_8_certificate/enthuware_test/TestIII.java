5826db83bc1759d1b264b9d877a7bdc3016997ad
Eren Kaya <erenkaya1993@gmail.com>
2019-12-15 11:52:04 +0300

    testIV passed

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/enthuware_test/TestIII.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/enthuware_test/TestIII.java
@@ -3,10 +3,9 @@ package kim.eren.oca_8_certificate.enthuware_test;
 public class TestIII {
 
     // test done i have failed this testIII 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int i = 0;
             String String = new String("asd");
-            
             System.out.println("args null"+args.length);
         
         if(i++<1){
@@ -15,6 +14,12 @@ public class TestIII {
         }else{
             System.out.println("checks first then other statement value increments");
         }
-        
+        nullExceptionTest();
+    }
+    // you can throw exception instance as a null 
+    // but e couldn't throw then null pointer exception occured
+    public static void nullExceptionTest() throws Exception{
+        Exception e = null;
+        throw e;
     }
 }
---
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/enthuware_test/TestIV.java
@@ -0,0 +1,12 @@
+package kim.eren.oca_8_certificate.enthuware_test;
+
+public class TestIV {
+
+    /**
+     * TestIV done i passed that one
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+}
