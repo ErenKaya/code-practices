e6fe1e296c06e934409916c6456720fb46b51ea2
Eren Kaya <erenkaya1993@gmail.com>
2019-12-20 07:42:39 +0300

    this keyword use in interface

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterV/topicII/default_interfaces/B.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterV/topicII/default_interfaces/B.java
@@ -1,5 +1,9 @@
 package kim.eren.oca_8_certificate.mala_gupta_guide.chapterV.topicII.default_interfaces;
 
 public interface B{
+    default void testThisKeyword(){
+        System.out.println(this);
+        this.print();
+    }
     abstract void print();
 }
--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterV/topicII/default_interfaces/TestDefaultMethod.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterV/topicII/default_interfaces/TestDefaultMethod.java
@@ -3,7 +3,7 @@ package kim.eren.oca_8_certificate.mala_gupta_guide.chapterV.topicII.default_int
 public class TestDefaultMethod implements A {
     public static void main(String[] args) {
         B a = new TestDefaultMethod();
-        a.print();
+        a.testThisKeyword();
     }
 
     @Override
