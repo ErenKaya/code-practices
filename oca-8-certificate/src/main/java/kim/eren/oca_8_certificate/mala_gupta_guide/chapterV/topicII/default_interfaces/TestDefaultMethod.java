22441480f0eea169300b15c2cc15147d328b768c
Eren Kaya <erenkaya1993@gmail.com>
2019-12-23 08:04:56 +0300

    search some documents

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterV/topicII/default_interfaces/TestDefaultMethod.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterV/topicII/default_interfaces/TestDefaultMethod.java
@@ -1,6 +1,7 @@
 package kim.eren.oca_8_certificate.mala_gupta_guide.chapterV.topicII.default_interfaces;
 
 public class TestDefaultMethod implements A {
+    //searching some documents
     public static void main(String[] args) {
         B a = new TestDefaultMethod();
         a.testThisKeyword();
