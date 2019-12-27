463328fe9507dc3d5b319da5af545c9b41140a5f
Eren Kaya <erenkaya1993@gmail.com>
2019-12-27 07:16:05 +0300

    lambda

--- oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterV/Practice.java
+++ oca-8-certificate/src/main/java/kim/eren/oca_8_certificate/mala_gupta_guide/chapterV/Practice.java
@@ -2,6 +2,6 @@ package kim.eren.oca_8_certificate.mala_gupta_guide.chapterV;
 
 public class Practice{
     public static void main(String[] args) {
-        
+        // removeIf, Predicate in lambda
     }
 }
