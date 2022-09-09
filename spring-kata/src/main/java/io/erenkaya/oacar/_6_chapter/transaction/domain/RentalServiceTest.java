8d76431de091447ff2f2d7e22941f8c73983fc93
ErenKaya <erenkaya1993@gmail.com>
2022-09-09 23:25:14 +0300

    new test added

--- spring-kata/src/main/java/io/erenkaya/oacar/_6_chapter/transaction/domain/RentalServiceTest.java
+++ spring-kata/src/main/java/io/erenkaya/oacar/_6_chapter/transaction/domain/RentalServiceTest.java
@@ -15,32 +15,40 @@ import io.erenkaya.oacar.domain.RentalService;
 
 public class RentalServiceTest {
 
-	public static void main(String[] args) throws ParseException {
-		RentalServiceTest test = new RentalServiceTest();
-		String path = "oacar\\bean7.xml";
-		ApplicationContext factory = new ClassPathXmlApplicationContext(path);
+    public static void main(String[] args) throws ParseException {
+        RentalServiceTest test = new RentalServiceTest();
+        String path = "oacar\\bean7.xml";
+        ApplicationContext factory = new ClassPathXmlApplicationContext(path);
 //		test.add_new_car();
-		test.add_new_car(factory);
-	}
+        test.find_customer(factory);
+    }
 
-	private void add_new_car() throws ParseException {
-		Car car = new Car("Ford Kuga");
-		RentalService service = new RentalServiceImpl(new CustomerRepositoryImpl(), new RentalRepositoryImpl());
-		Date rentalBegin = new SimpleDateFormat("dd/MM/yy").parse("22/12/2013");
-		Date rentalEnd = new SimpleDateFormat("dd/MM/yy").parse("23/12/2013");
-		Customer customer = new Customer("Kaya", 22, "Eren", ThreadLocalRandom.current().nextLong());
-		Rental rental = service.rentACar(customer, car, rentalBegin, rentalEnd);
-		System.out.println("is rental rented? " + rental.isRented());
-	}
+    private void add_new_car() throws ParseException {
+        Car car = new Car("Ford Kuga");
+        RentalService service = new RentalServiceImpl(new CustomerRepositoryImpl(), new RentalRepositoryImpl());
+        Date rentalBegin = new SimpleDateFormat("dd/MM/yy").parse("22/12/2013");
+        Date rentalEnd = new SimpleDateFormat("dd/MM/yy").parse("23/12/2013");
+        Customer customer = new Customer("Kaya", 22, "Eren", ThreadLocalRandom.current().nextLong());
+        Rental rental = service.rentACar(customer, car, rentalBegin, rentalEnd);
+        System.out.println("is rental rented? " + rental.isRented());
+    }
 
-	private void add_new_car(ApplicationContext context) throws ParseException {
-		Car car = new Car("Ford Kuga");
-		RentalService service = (RentalService) context.getBean("rentalService");
-		Date rentalBegin = new SimpleDateFormat("dd/MM/yy").parse("22/12/2013");
-		Date rentalEnd = new SimpleDateFormat("dd/MM/yy").parse("23/12/2013");
-		Customer customer = new Customer("Kaya", 22, "Eren", ThreadLocalRandom.current().nextLong());
-		Rental rental = service.rentACar(customer, car, rentalBegin, rentalEnd);
-		System.out.println("is rental rented? " + rental.isRented());
-	}
+    private void find_customer(ApplicationContext context) throws ParseException {
+        Car car = new Car("Ford Kuga");
+        JDBCCustomerRepositoryImpl service = (JDBCCustomerRepositoryImpl) context.getBean("jDBCCustomerRepositoryImpl");
+        Customer customer = service.getCustomerByName("Isik");
+        System.out.println("is rental rented? " + customer.getAge());
+    }
+
+
+    private void add_new_car(ApplicationContext context) throws ParseException {
+        Car car = new Car("Ford Kuga");
+        RentalService service = (RentalService) context.getBean("rentalService");
+        Date rentalBegin = new SimpleDateFormat("dd/MM/yy").parse("22/12/2013");
+        Date rentalEnd = new SimpleDateFormat("dd/MM/yy").parse("23/12/2013");
+        Customer customer = new Customer("Kaya", 22, "Eren", ThreadLocalRandom.current().nextLong());
+        Rental rental = service.rentACar(customer, car, rentalBegin, rentalEnd);
+        System.out.println("is rental rented? " + rental.isRented());
+    }
 
 }
